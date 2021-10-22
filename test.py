1d72c054acf566473e7875abf2093582d9508328
MilesKidson <miles.kidson@gmail.com>
2021-10-22 17:02:01 +0200

    change test.py output to png

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import dso1kb
+import numpy as np
+import matplotlib.pyplot as plt
+
 dso=dso1kb.Dso("10.10.0.20:3001")
 dso.getRawData(True, 1)
 fwave = []
 fwave = dso.convertWaveform(1, 1)
-# print(fwave)
-f = open('testData.csv', 'w')
-f.write(fwave)
-f.close()
 
+tArr = np.arange(0,len(fwave))
+plt.plot(tArr, fwave)
+plt.savefig('testplot.png')
