0f2522aab57cf03a19103e5b041d30b9c63cae8f
MilesKidson <miles.kidson@gmail.com>
2021-10-23 15:30:41 +0200

    renamed back to dso1kb

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-import dso1kb_1
+import dso1kb
 import numpy as np
 import matplotlib.pyplot as plt
 
-dso=dso1kb_1.Dso("10.10.0.20:3001")
+dso=dso1kb.Dso("10.10.0.20:3001")
 dso.getRawData(True, 1)
 fwave = []
 fwave = dso.convertWaveform(1, 1)
