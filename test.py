e78b46fb661356965885818c93ca33c0b1572deb
MilesKidson <miles.kidson@gmail.com>
2021-10-23 16:28:04 +0200

    changed test.py to do all 3 channels

--- test.py
+++ test.py
@@ -4,9 +4,18 @@ import matplotlib.pyplot as plt
 
 dso=dso1kb.Dso("10.10.0.20:3001")
 dso.getRawData(True, 1)
-fwave = []
-fwave = dso.convertWaveform(1, 1)
+fwave1 = []
+fwave1 = dso.convertWaveform(1, 1)
+dso.getRawData(True, 2)
+fwave2 = []
+fwave2 = dso.convertWaveform(2, 1)
+dso.getRawData(True, 3)
+fwave3 = []
+fwave3 = dso.convertWaveform(3, 1)
 
-tArr = np.arange(0,len(fwave))
-plt.plot(tArr, fwave)
+tArr = np.arange(0,len(fwave1))
+plt.plot(tArr, fwave1, label='ch1')
+plt.plot(tArr, fwave2, label='ch2')
+plt.plot(tArr, fwave3, label='ch3')
+plt.legend()
 plt.savefig('testplot.png')
