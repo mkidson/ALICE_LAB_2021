b2dc59533712477a681c1d79bbabb9e056a20b7d
MilesKidson <miles.kidson@gmail.com>
2021-10-25 13:03:03 +0200

    made time resolution program. also made sure mockup final doesn't overwrite data

--- timeResolution.py
+++ timeResolution.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """
-A mockup of the program I think should be run when we want to take a run of data. It should run through the number of events we want to take, saving the relevant data at each step before moving to the next event. 
+Program designed to be able to take a number of events from just the scintillators in order to make time resolution measurements.
+It takes both the run number of number of events as arguments when running the program, and should cancel the run if a run of that number already exists, just to avoid writing over data.
 """
+
 import dso1kb
 import datetime
 import os
@@ -29,32 +31,27 @@ if args.printargs:
 dso = dso1kb.Dso('10.10.0.20:3001', True)
 
 # Makes a new directory for the data. You will need to change the path to this for when you save data. This checks if the run exists and if it does, exits, else it creates a directory for the data and carries on
-if os.system(f'test -d ~/prac2021/data/oscilloscopeData/run_{args.run}') == 0:
+if os.system(f'test -d ~/prac2021/data/timeResolutionData/run_{args.run}') == 0:
     print('That run already exists, change run number to avoid writing over data')
     exit(0)
 else:
-    os.system(f'mkdir ~/prac2021/data/oscilloscopeData/run_{args.run}')
+    os.system(f'mkdir ~/prac2021/data/timeResolutionData/run_{args.run}')
 # similar thing for TRD data 
 
-for i in range(int(args.n_events)):
+for i in range(args.n_events):
     now = datetime.datetime.now()
     nowString = now.strftime('%Y.%m.%d.%H.%M.%S')
 
-    # unblock trigger
-    # read event (minidaq)
-    # could even run the TRD and scope data collection in parallel. wouldn't be too hard to code in.
-    
-    # Gets the raw data from the oscilloscope, which then gets saved to a variable in the dso object
     dso.getRawData(True, 1)
     dso.getRawData(True, 2)
-    dso.getRawData(True, 3)
+    # dso.getRawData(True, 3)
 
     waveform = []
 
     # The convertWaveForm function takes the raw data from the dso object, formats it as a list of floats, and then returns that list
     waveform.append(dso.convertWaveform(1, 1))
     waveform.append(dso.convertWaveform(2, 1))
-    waveform.append(dso.convertWaveform(3, 1))
+    # waveform.append(dso.convertWaveform(3, 1))
 
     waveform = np.array(waveform)
     np.savetxt(f'~/prac2021/data/oscilloscopeData/run_{args.run}/{i}.csv', waveform, header=nowString, delimiter=',')
