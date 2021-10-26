4d4c98f9fd94b4a6ec56d1910f8e7e2f47e75877
MilesKidson <miles.kidson@gmail.com>
2021-10-26 23:31:35 +0200

    add screenshot to package

---
+++ oscilloscope/screenshot.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Takes a screeshot of the current oscilloscope screen and saves it to the folder under the name given by the user.
+"""
+import matplotlib.pyplot as plt
+
+import dso1kb
+import os
+
+dso=dso1kb.Dso('/dev/ttyACM2')
+
+dso.write(':DISP:OUTP?\n')
+dso.getBlockData()
+dso.ImageDecode(1)
+
+plt.imshow(dso.im)
+saveName = str(input('Enter the name the screenshot will be saved under: '))
+# requires a screenshots folder in the outer directory
+plt.savefig(f'screenshots/{saveName}.png')
